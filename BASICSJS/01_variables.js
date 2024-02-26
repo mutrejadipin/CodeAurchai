3eb0e3d83bf1ed97bd460f25b498b3c5fdcb6732
mutrejadipin <88284088+mutrejadipin@users.noreply.github.com>
2024-02-26 14:46:35 +0000

    L1Variables

---
+++ BASICSJS/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=1122
+let accountEmail="mutreja@google.com"
+var accountPassword="12Ab" /**not used frequetly because of its issue in block scope and functional scope */
+accountCity="Gurgaon"
+let accountState;
+
+console.log(accountID)
+//accountID=1112
+//console.log(accountID) -> because const cant change
+console.table([accountID,accountEmail,accountPassword,accountCity]) //console.table is useful when print multiple values at a time in tabular format
+accountEmail="dipin@microsoft.com"
+accountPassword="12aB"
+accountCity="Noida"
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
