1eac1fa9ee7a00abbf518f677c9802a38c36742d
mutrejadipin <88284088+mutrejadipin@users.noreply.github.com>
2024-02-28 14:31:22 +0000

    Data type conversions

---
+++ BASICSJS/03_conversionOperations.js
@@ -0,0 +1,32 @@
+//Data type conversion
+let age=10
+age="33"
+let score="ab12"
+
+console.log(typeof age)
+console.log(typeof score)
+let Numage = Number(age)
+let Numscore = Number(score)
+console.log(typeof Numage)
+console.log(Numage)
+console.log(typeof Numscore)
+console.log(Numscore)
+
+let name=null
+console.log(name)
+console.log(typeof name)
+let numname=Number(name)
+console.log(numname)
+
+let check = true
+let numcheck=Number(check)
+console.log(numcheck)
+
+let name2="Dipin"
+let numname2=Number(name2)
+console.log(numname2)
+
+let boolname2=Boolean(name2)
+console.log(boolname2)
+
+
